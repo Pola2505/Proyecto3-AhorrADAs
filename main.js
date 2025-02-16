e9a1d7cad6b0be5b8a5e934fd689d2cc71a538f7
rociolobruno17 <rociolobruno1@gmail.com>
2025-02-16 13:58:49 -0300

    agregue la funcionalidad al boton de mostrar y ocultar filtros

---
+++ main.js
@@ -0,0 +1,22 @@
+function $(element) {
+  return document.querySelector(element)
+}
+
+// Estos son todos mis elementos de HTML
+const $botonFiltros = $("#boton-filtros")
+const $filtros = $("#filtros-contenido")
+
+
+// Función para mostrar y ocultar filtros
+document.addEventListener("DOMContentLoaded", function () {
+  
+    $botonFiltros.addEventListener("click", function () {
+      if ($filtros.classList.contains("hidden")) {
+        $filtros.classList.remove("hidden");
+        $botonFiltros.textContent = "Ocultar filtros";
+      } else {
+        $filtros.classList.add("hidden");
+        $botonFiltros.textContent = "Mostrar filtros";
+      }
+    });
+  });
