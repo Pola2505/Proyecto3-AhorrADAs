fdd8ccec2ab3629d1c35ac71229bfa49df5f89d4
Sandra <sandra27popielarczyk@gmail.com>
2025-02-20 18:15:30 -0300

    pequenos ajustes

--- main.js
+++ main.js
@@ -8,14 +8,28 @@ const $$ = (element) => document.querySelectorAll(element);
 const $menu = $('#menu');
 const $menuIcono = $('#menu-icono');
 const $navCategorias = $('#nav-categorias');
+const $navReportes = $('#nav-reportes');
 const $categorias = $('#categorias-container');
+const $reportes = $('#reportes-container');
+
+// El menu mobile icono 
 
 $menuIcono.addEventListener('click', () => {
     $menu.classList.toggle('hidden');
 })
 
+// Es necesario poner el evento preventDefault para todos los links del menu ya que el href hace que la pagina recarga !!! 
+
 $navCategorias.addEventListener('click', (event) => {
     event.preventDefault();
     $categorias.classList.toggle('hidden');
+    $reportes.classList.add('hidden');
 })
+
+$navReportes.addEventListener('click', (event) => {
+    event.preventDefault();
+    $categorias.classList.add('hidden');
+    $reportes.classList.toggle('hidden');
+})
+
 
