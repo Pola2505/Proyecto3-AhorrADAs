a80c755ee7967ad37bd042a56368688c9c369e86
Sandra <sandra27popielarczyk@gmail.com>
2025-03-03 23:34:34 -0300

    probando cosas

--- main.js
+++ main.js
@@ -213,9 +213,11 @@ function pintarDatos(array) {
   if (array.length === 0) {
     ocultarElemento([$columnasCategorias]);
     $operacionesCargadas.innerHTML = `
+    <div class="flex flex-col grow justify-center items-center">
     <img src="./imagenes/wallet.svg" alt="Ilustración de billetera" class="w-40 sm:w-60">
     <p class="text-2xl text-textPrimary mt-4 mb-2">Sin resultados</p>
     <p class="text-1xl text-textPrimary mb-6"> Cambia los filtros o agrega operaciones </p>
+    </div>
     `
   } else {
     $operacionesCargadas.innerHTML = array.map(operacion => {
